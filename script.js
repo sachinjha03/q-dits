d5f3104d7da513c68a51717033c1afe3cf49e9e5
Sachin Jha <sachin03.nic@gmail.com>
2024-05-08 10:22:49 +0530

    landing section + navbar

--- script.js
+++ script.js
@@ -3,7 +3,9 @@ document.addEventListener("DOMContentLoaded", function() {
     const images = document.querySelectorAll(".random-image");
   
     images.forEach(function(image) {
-      const randomX = Math.random() * (container.offsetWidth - image.offsetWidth);
+      // const randomX = Math.random() * (container.offsetWidth - image.offsetWidth);
+      // const randomY = Math.random() * (container.offsetHeight - image.offsetHeight);
+      const randomX = Math.random() * 500;
       const randomY = Math.random() * (container.offsetHeight - image.offsetHeight);
       
       image.style.left = randomX + "px";
